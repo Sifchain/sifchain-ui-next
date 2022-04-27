850a44333f11c6765808136852d38a69d93b2446
Alan <alanrsoars@gmail.com>
2022-04-27 14:07:29 +1200

    chore: scaffold initial folder structure

---
+++ apps/dex/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/compounds/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
