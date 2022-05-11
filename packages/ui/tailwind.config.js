72df18e84fb89a937a7bb4f8693ba5b90e4d8ff7
Alan <alanrsoars@gmail.com>
2022-05-11 23:22:01 +1200

    ui: expose tailwind.config.preset

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,7 +1,9 @@
+const sifchainPreset = require("./tailwind.config.preset");
 /**
  * @type {import("tailwindcss/tailwind-config").TailwindConfig}
  */
 module.exports = {
+  presets: [sifchainPreset],
   content: ["./src/**/*.{jsx,tsx,mdx}"],
   theme: {
     extend: {},
