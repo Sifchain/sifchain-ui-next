f11df8dc0ea83674f4aaa85ed0c903b4322addc4
Alan <alanrsoars@gmail.com>
2022-05-12 15:02:22 +1200

    chore: use next-transpile-modules on @sifchain/docs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,14 @@
+const withTM = require("next-transpile-modules")(["@sifchain/ui"]);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 });
-module.exports = withNextra();
+
+module.exports = withNextra(withTM(nextConfig));
