f8bd6c3266010b2e8acb43d4c472f5f06a5b82d4
Alan <alanrsoars@gmail.com>
2022-05-12 17:04:42 +1200

    chore: tweak button component

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -10,7 +10,7 @@ const Button: FC<Props> = (props) => (
   <button
     {...props}
     className={clsx("bg-sifgray-200 rounded-lg place-items-center", {
-      "bg-zinc-600 dark:bg-slate-300 text-sifgray-50":
+      "bg-indigo-600 dark:bg-slate-300 text-sifgray-50":
         props.variant === "primary",
       "text-xs py-1 px-1.5": props.size === "xs",
       "text-sm py-1.5 px-2.5": props.size === "sm",
