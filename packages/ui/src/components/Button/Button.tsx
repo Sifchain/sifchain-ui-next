fdbf7d5c46a3f5481a61e40423f43c8cb0de5961
Alan <alanrsoars@gmail.com>
2022-05-19 12:17:19 +1200

    ui: Tooltip component + upgrade sotrybook

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,25 +1,28 @@
 import clsx from "clsx";
-import { FC } from "react";
+import { forwardRef } from "react";
 
 export type ButtonProps = JSX.IntrinsicElements["button"] & {
   size?: "xs" | "sm" | "md" | "lg";
   variant?: "primary" | "secondary" | "tertiary";
 };
 
-export const Button: FC<ButtonProps> = (props) => (
-  <button
-    {...props}
-    className={clsx("bg-sifgray-200 rounded-lg place-items-center", {
-      "bg-indigo-600 dark:bg-slate-300 text-sifgray-50":
-        props.variant === "primary",
-      "text-xs py-1 px-1.5": props.size === "xs",
-      "text-sm py-1.5 px-2.5": props.size === "sm",
-      "text-base py-2 px-3": props.size === "md",
-      "text-lg py-2.5 px-4": props.size === "lg",
-    })}
-  >
-    {props.children}
-  </button>
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => (
+    <button
+      {...props}
+      ref={ref}
+      className={clsx("bg-sifgray-200 rounded-lg place-items-center", {
+        "bg-indigo-600 dark:bg-slate-300 text-sifgray-50":
+          props.variant === "primary",
+        "text-xs py-1 px-1.5": props.size === "xs",
+        "text-sm py-1.5 px-2.5": props.size === "sm",
+        "text-base py-2 px-3": props.size === "md",
+        "text-lg py-2.5 px-4": props.size === "lg",
+      })}
+    >
+      {props.children}
+    </button>
+  ),
 );
 
 Button.defaultProps = {
