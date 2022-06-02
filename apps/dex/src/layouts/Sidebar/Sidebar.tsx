95900118be63c7f511cf4f20ad8dbcf1d3145cfd
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-02 14:44:29 +1200

    fix: qr code appear below dialog

--- apps/dex/src/layouts/Sidebar/Sidebar.tsx
+++ apps/dex/src/layouts/Sidebar/Sidebar.tsx
@@ -260,7 +260,7 @@ const WalletChooserModal = (props: {
 }) => {
   return (
     <Transition appear show={props.visible} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={props.onCloseRequest}>
+      <Dialog onClose={props.onCloseRequest}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -270,9 +270,8 @@ const WalletChooserModal = (props: {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <Dialog.Backdrop className="fixed inset-0 bg-black bg-opacity-50" />
         </Transition.Child>
-
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
