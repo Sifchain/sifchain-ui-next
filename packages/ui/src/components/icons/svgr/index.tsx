341b2e2c49682117bc0fa1a7406c4eb2dee3eb1f
Alan <alanrsoars@gmail.com>
2022-06-07 16:38:38 +1200

    chore: add more icons

--- packages/ui/src/components/icons/svgr/index.tsx
+++ packages/ui/src/components/icons/svgr/index.tsx
@@ -1,11 +1,15 @@
 export { default as ArrowLeftIcon } from "./ArrowLeftIcon";
 export { default as AvalancheIcon } from "./AvalancheIcon";
 export { default as BalanceIcon } from "./BalanceIcon";
+export { default as BscIcon } from "./BscIcon";
 export { default as ChangelogIcon } from "./ChangelogIcon";
+export { default as CoinbaseIcon } from "./CoinbaseIcon";
 export { default as EthereumIcon } from "./EthereumIcon";
 export { default as LockIcon } from "./LockIcon";
+export { default as PolygonIcon } from "./PolygonIcon";
 export { default as PoolsIcon } from "./PoolsIcon";
 export { default as RowanIcon } from "./RowanIcon";
 export { default as SearchIcon } from "./SearchIcon";
 export { default as SwapIcon } from "./SwapIcon";
+export { default as TerraIcon } from "./TerraIcon";
 export { default as WalletIcon } from "./WalletIcon";
