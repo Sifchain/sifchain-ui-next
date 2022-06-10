12aff3df6f6f9fefc1468778fb2d5160b9af26ca
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-10 12:50:58 +1200

    test: reverse swap

--- packages/math/src/formulae/swap.spec.ts
+++ packages/math/src/formulae/swap.spec.ts
@@ -1,38 +1,44 @@
-import { calculatePmtpSwapResult, calculateSwapResult } from "./swap";
+import {
+  calculatePmtpSwapResult,
+  calculateReverseSwapResult,
+  calculateSwapResult,
+} from "./swap";
 
 import BigNumber from "bignumber.js";
-import swapResultFixture from "../../__fixtures__/single_swap_result.json";
+import pmtpSwapFixture from "../../__fixtures__/pmtp_swap.json";
+import reverseSwapFixture from "../../__fixtures__/reverse_swap.json";
+import swapFixture from "../../__fixtures__/swap.json";
 
 describe("swap", () => {
-  test.each(
-    swapResultFixture.SingleSwapResult.map((x) => [x.x, x.X, x.Y, x.expected]),
-  )("swapping %s for %s with balance %s returns %s", (x, X, Y, expected) => {
-    const output = calculateSwapResult(x, X, Y);
-    expect(output.toPrecision(18)).toEqual(
-      new BigNumber(expected).toPrecision(18),
-    );
-  });
-});
-
-describe("swap with PMTP", () => {
-  test.each(
-    swapResultFixture.SingleSwapResult_pmtp.map((x) => [
-      x.x,
-      x.X,
-      x.Y,
-      x.A,
-      x.expected,
-    ]),
-  )(
-    "swapping %s for %s with balance %s and adjustment of %s returns %s",
-    (x, X, Y, A, expected) => {
-      const [$x, $X, $Y, $A] = [x, X, Y, A].map((x) => new BigNumber(x));
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const output = calculatePmtpSwapResult($x!, $X!, $Y!, $A!);
-
+  test.each(swapFixture.map((x) => [x.x, x.X, x.Y, x.expected]))(
+    "swapping %s for %s with balance %s returns %s",
+    (x, X, Y, expected) => {
+      const output = calculateSwapResult(x, X, Y);
       expect(output.toPrecision(18)).toEqual(
         new BigNumber(expected).toPrecision(18),
       );
+    },
+  );
+});
+
+describe("swap with PMTP", () => {
+  test.each(pmtpSwapFixture.map((x) => [x.x, x.X, x.Y, x.A, x.expected]))(
+    "swapping %s for %s with balance %s and adjustment of %s returns %s",
+    (x, X, Y, A, expected) => {
+      const output = calculatePmtpSwapResult(x, X, Y, A);
+      expect(output.toPrecision(18)).toEqual(
+        new BigNumber(expected).toPrecision(18),
+      );
+    },
+  );
+});
+
+describe("reverse swap", () => {
+  test.each(reverseSwapFixture.map((x) => [x.S, x.X, x.Y, x.expected]))(
+    "reverse swapping %s for %s with balance %s returns %s",
+    (S, X, Y, expected) => {
+      const x = calculateReverseSwapResult(S, X, Y);
+      expect(x.toPrecision(18)).toBe(new BigNumber(expected).toPrecision(18));
     },
   );
 });
