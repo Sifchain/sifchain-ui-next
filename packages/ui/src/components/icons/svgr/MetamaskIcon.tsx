ed2b27b75a29e7e5579b7c1ab31f2cfef33cd0bc
Alan <alanrsoars@gmail.com>
2022-06-13 17:59:54 +1200

    chore: add more terrastation icon

--- packages/ui/src/components/icons/svgr/MetamaskIcon.tsx
+++ packages/ui/src/components/icons/svgr/MetamaskIcon.tsx
@@ -2,83 +2,108 @@ import type { SVGProps } from "react";
 
 const SvgMetamaskIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    id="metamask-icon_svg__Layer_1"
+    viewBox="0 0 22 20"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    x={0}
-    y={0}
-    viewBox="0 0 318.6 318.6"
-    xmlSpace="preserve"
     width="1em"
     height="1em"
     {...props}
   >
-    <style>
-      {
-        ".metamask-icon_svg__st1,.metamask-icon_svg__st2,.metamask-icon_svg__st3,.metamask-icon_svg__st4,.metamask-icon_svg__st5,.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#e4761b;stroke:#e4761b;stroke-linecap:round;stroke-linejoin:round}.metamask-icon_svg__st2,.metamask-icon_svg__st3,.metamask-icon_svg__st4,.metamask-icon_svg__st5,.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#d7c1b3;stroke:#d7c1b3}.metamask-icon_svg__st3,.metamask-icon_svg__st4,.metamask-icon_svg__st5,.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#233447;stroke:#233447}.metamask-icon_svg__st4,.metamask-icon_svg__st5,.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#cd6116;stroke:#cd6116}.metamask-icon_svg__st5,.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#e4751f;stroke:#e4751f}.metamask-icon_svg__st6,.metamask-icon_svg__st9{fill:#f6851b;stroke:#f6851b}.metamask-icon_svg__st9{fill:#763d16;stroke:#763d16}"
-      }
-    </style>
     <path
-      style={{
-        fill: "#e2761b",
-        stroke: "#e2761b",
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-      }}
-      d="m274.1 35.5-99.5 73.9L193 65.8z"
+      d="m20.011.752-7.813 5.781 1.453-3.407L20.01.752Z"
+      fill="#E17726"
+      stroke="#E17726"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st1"
-      d="m44.4 35.5 98.7 74.6-17.5-44.3zM238.3 206.8l-26.5 40.6 56.7 15.6 16.3-55.3zM33.9 207.7 50.1 263l56.7-15.6-26.5-40.6z"
+      d="m1.99.752 7.743 5.835-1.384-3.46L1.99.751ZM17.198 14.158l-2.08 3.175 4.452 1.226 1.275-4.332-3.647-.07ZM1.162 14.227l1.267 4.332 4.444-1.226-2.071-3.175-3.64.069Z"
+      fill="#E27625"
+      stroke="#E27625"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st1"
-      d="m103.6 138.2-15.8 23.9 56.3 2.5-2-60.5zM214.9 138.2l-39-34.8-1.3 61.2 56.2-2.5zM106.8 247.4l33.8-16.5-29.2-22.8zM177.9 230.9l33.9 16.5-4.7-39.3z"
+      d="m6.634 8.792-1.237 1.866 4.405.2-.147-4.74-3.021 2.674ZM15.366 8.793l-3.068-2.729-.1 4.794 4.405-.2-1.237-1.865ZM6.873 17.333l2.666-1.287-2.295-1.789-.37 3.076ZM12.46 16.046l2.659 1.287-.363-3.076-2.295 1.789Z"
+      fill="#E27625"
+      stroke="#E27625"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st2"
-      d="m211.8 247.4-33.9-16.5 2.7 22.1-.3 9.3zM106.8 247.4l31.5 14.9-.2-9.3 2.5-22.1z"
+      d="m15.119 17.331-2.658-1.287.216 1.727-.023.732 2.465-1.172ZM6.873 17.331l2.473 1.172-.015-.732.208-1.727-2.666 1.287Z"
+      fill="#D5BFB2"
+      stroke="#D5BFB2"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st3"
-      d="m138.8 193.5-28.2-8.3 19.9-9.1zM179.7 193.5l8.3-17.4 20 9.1z"
+      d="m9.392 13.117-2.21-.648 1.561-.717.65 1.365ZM12.607 13.117l.65-1.365 1.568.717-2.217.648Z"
+      fill="#233447"
+      stroke="#233447"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st4"
-      d="m106.8 247.4 4.8-40.6-31.3.9zM207 206.8l4.8 40.6 26.5-39.7zM230.8 162.1l-56.2 2.5 5.2 28.9 8.3-17.4 20 9.1zM110.6 185.2l20-9.1 8.2 17.4 5.3-28.9-56.3-2.5z"
+      d="m6.873 17.333.387-3.175-2.458.069 2.071 3.106ZM14.74 14.158l.379 3.175 2.079-3.106-2.458-.07ZM16.603 10.658l-4.405.2.41 2.259.649-1.365 1.569.717 1.777-1.811ZM7.182 12.47l1.561-.718.65 1.365.409-2.259-4.405-.2 1.785 1.811Z"
+      fill="#CC6228"
+      stroke="#CC6228"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st5"
-      d="m87.8 162.1 23.6 46-.8-22.9zM208.1 185.2l-1 22.9 23.7-46zM144.1 164.6l-5.3 28.9 6.6 34.1 1.5-44.9zM174.6 164.6l-2.7 18 1.2 45 6.7-34.1z"
+      d="m5.398 10.658 1.847 3.6-.062-1.789-1.785-1.811ZM14.825 12.47l-.07 1.788 1.848-3.6-1.778 1.811ZM9.802 10.858l-.41 2.258.518 2.667.116-3.515-.224-1.41ZM12.198 10.858l-.217 1.403.108 3.522.518-2.667-.41-2.258Z"
+      fill="#E27525"
+      stroke="#E27525"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st6"
-      d="m179.8 193.5-6.7 34.1 4.8 3.3 29.2-22.8 1-22.9zM110.6 185.2l.8 22.9 29.2 22.8 4.8-3.3-6.6-34.1z"
+      d="m12.608 13.116-.518 2.667.37.262 2.296-1.789.07-1.788-2.218.648ZM7.182 12.468l.062 1.788 2.295 1.789.371-.263-.518-2.666-2.21-.648Z"
+      fill="#F5841F"
+      stroke="#F5841F"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      style={{
-        fill: "#c0ad9e",
-        stroke: "#c0ad9e",
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-      }}
-      d="m180.3 262.3.3-9.3-2.5-2.2h-37.7l-2.3 2.2.2 9.3-31.5-14.9 11 9 22.3 15.5h38.3l22.4-15.5 11-9z"
+      d="m12.654 18.503.023-.732-.201-.17H9.524l-.193.17.015.732-2.473-1.172.866.71 1.754 1.21h3.006l1.762-1.21.858-.71-2.465 1.172Z"
+      fill="#C0AC9D"
+      stroke="#C0AC9D"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      style={{
-        fill: "#161616",
-        stroke: "#161616",
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-      }}
-      d="m177.9 230.9-4.8-3.3h-27.7l-4.8 3.3-2.5 22.1 2.3-2.2h37.7l2.5 2.2z"
+      d="m12.46 16.046-.37-.262H9.91l-.37.262-.209 1.727.193-.17h2.952l.201.17-.216-1.727Z"
+      fill="#161616"
+      stroke="#161616"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st9"
-      d="m278.3 114.2 8.5-40.8-12.7-37.9-96.2 71.4 37 31.3 52.3 15.3 11.6-13.5-5-3.6 8-7.3-6.2-4.8 8-6.1zM31.8 73.4l8.5 40.8-5.4 4 8 6.1-6.1 4.8 8 7.3-5 3.6 11.5 13.5 52.3-15.3 37-31.3-96.2-71.4z"
+      d="M20.343 6.91 21 3.72 20.01.752 12.46 6.34l2.906 2.452 4.104 1.194.904-1.056-.394-.285.626-.57-.48-.37.627-.478-.41-.316ZM1 3.72l.665 3.19-.425.317.633.478-.479.37.626.57-.394.285.904 1.056 4.104-1.194L9.539 6.34 1.99.752 1 3.72Z"
+      fill="#763E1A"
+      stroke="#763E1A"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
-      className="metamask-icon_svg__st6"
-      d="m267.2 153.5-52.3-15.3 15.9 23.9-23.7 46 31.2-.4h46.5zM103.6 138.2l-52.3 15.3-17.4 54.2h46.4l31.1.4-23.6-46zM174.6 164.6l3.3-57.7 15.2-41.1h-67.5l15 41.1 3.5 57.7 1.2 18.2.1 44.8h27.7l.2-44.8z"
+      d="m19.47 9.987-4.104-1.195 1.237 1.865-1.847 3.6 2.442-.03h3.648l-1.376-4.24ZM6.633 8.792 2.53 9.987l-1.368 4.24h3.64l2.442.03-1.847-3.6 1.236-1.865ZM12.198 10.858l.263-4.517 1.19-3.214H8.349L9.54 6.34l.263 4.517.1 1.418.008 3.508h2.18l.007-3.508.1-1.418Z"
+      fill="#F5841F"
+      stroke="#F5841F"
+      strokeWidth={0.149}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
---
+++ packages/ui/src/components/icons/svgr/SortUnderterminedIcon.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from "react";
+
+const SvgSortUnderterminedIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 8 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="m8 12-4 4-4-4h8ZM0 4l4-4 4 4H0Z" fill="#9699A5" />
+  </svg>
+);
+
+export default SvgSortUnderterminedIcon;
