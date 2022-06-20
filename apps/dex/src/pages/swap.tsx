3f98fc734527ba13380b514c9a1cb45999282c52
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-21 07:31:08 +1200

    feat: shrink swap card on larger screen

--- apps/dex/src/pages/swap.tsx
+++ apps/dex/src/pages/swap.tsx
@@ -138,115 +138,117 @@ const SwapPage = () => {
   );
 
   return (
-    <section className="flex-1 flex flex-col bg-gray-800 w-full h-full p-6">
-      <header className="flex items-center justify-between pb-6">
-        <h2 className="text-2xl font-bold text-white">Swap</h2>
-        <Popover className="relative">
-          {({ open }) => (
-            <>
-              <Popover.Button>
-                {open ? <ArrowLeftIcon /> : <SettingsIcon />}
-              </Popover.Button>
-              <Transition
-                as={Fragment}
-                enter="transition duration-100 ease-out"
-                enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform scale-100 opacity-100"
-                leaveTo="transform scale-95 opacity-0"
-              >
-                <Popover.Panel className="absolute z-30 right-[-100%] bg-gray-700 rounded-lg border border-gray-600 p-4">
-                  <div className="flex flex-col">
-                    <legend className="float-left font-bold">Settings</legend>
-                    <div className="flex items-center">
-                      <label className="pr-6 text-sm opacity-90">
-                        Slippage
-                      </label>
-                      <ButtonGroup
-                        itemClassName="px-4"
-                        size="sm"
-                        gap={8}
-                        selectedIndex={selectedSlippageIndex}
-                        options={slippageOptions}
-                        onChange={setSelectedSlippageIndex}
-                      />
+    <div className="flex-1 flex justify-center items-center">
+      <section className="flex-1 flex flex-col bg-gray-800 w-full h-full md:rounded-xl md:flex-initial md:w-auto md:h-auto p-6">
+        <header className="flex items-center justify-between pb-6">
+          <h2 className="text-2xl font-bold text-white">Swap</h2>
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                <Popover.Button>
+                  {open ? <ArrowLeftIcon /> : <SettingsIcon />}
+                </Popover.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Popover.Panel className="absolute z-30 right-[-100%] bg-gray-700 rounded-lg border border-gray-600 p-4">
+                    <div className="flex flex-col">
+                      <legend className="float-left font-bold">Settings</legend>
+                      <div className="flex items-center">
+                        <label className="pr-6 text-sm opacity-90">
+                          Slippage
+                        </label>
+                        <ButtonGroup
+                          itemClassName="px-4"
+                          size="sm"
+                          gap={8}
+                          selectedIndex={selectedSlippageIndex}
+                          options={slippageOptions}
+                          onChange={setSelectedSlippageIndex}
+                        />
+                      </div>
                     </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
-      </header>
-      <form
-        className="flex-1 flex flex-col justify-between"
-        onSubmit={(event) => {
-          event.preventDefault();
-          swapMutation.mutate({
-            fromDenom,
-            toDenom,
-            fromAmount: fromAmountDecimal?.atomics ?? "0",
-            minimumReceiving: swapSimulationResult?.minimumReceiving ?? "0",
-          });
-        }}
-      >
-        <div className="flex flex-col gap-3">
-          <fieldset className="flex flex-col gap-2 bg-black rounded-md p-6">
-            <legend className="float-left font-bold opacity-90">From</legend>
-            <Select
-              className="relative z-20"
-              value={fromSelectedOption}
-              options={tokenOptions}
-              onChange={setFromSelectedOption}
-            />
-            <Input
-              placeholder="Swap amount"
-              value={fromAmount}
-              onChange={(event) => setFromAmount(event.target.value)}
-              fullWidth
-            />
-          </fieldset>
-          <div className="flex justify-center align-middle my-[-2em] z-10">
-            <button
-              className="bg-gray-900 rounded-full p-3 border-4 border-gray-800"
-              type="button"
-              onClick={() => {
-                // need this else gonna freeze the browser if user spam click
-                requestAnimationFrame(() => {
-                  setFromSelectedOption(toSelectedOption);
-                  setToSelectedOption(fromSelectedOption);
-                  setFromAmount((x) =>
-                    formattedResult.minimumReceiving === "0"
-                      ? x
-                      : formattedResult.minimumReceiving,
-                  );
-                });
-              }}
-            >
-              <SwapIcon width="1.25em" height="1.25em" />
-            </button>
+                  </Popover.Panel>
+                </Transition>
+              </>
+            )}
+          </Popover>
+        </header>
+        <form
+          className="flex-1 flex flex-col justify-between"
+          onSubmit={(event) => {
+            event.preventDefault();
+            swapMutation.mutate({
+              fromDenom,
+              toDenom,
+              fromAmount: fromAmountDecimal?.atomics ?? "0",
+              minimumReceiving: swapSimulationResult?.minimumReceiving ?? "0",
+            });
+          }}
+        >
+          <div className="flex flex-col gap-3">
+            <fieldset className="flex flex-col gap-2 bg-black rounded-md p-6">
+              <legend className="float-left font-bold opacity-90">From</legend>
+              <Select
+                className="relative z-20"
+                value={fromSelectedOption}
+                options={tokenOptions}
+                onChange={setFromSelectedOption}
+              />
+              <Input
+                placeholder="Swap amount"
+                value={fromAmount}
+                onChange={(event) => setFromAmount(event.target.value)}
+                fullWidth
+              />
+            </fieldset>
+            <div className="flex justify-center align-middle my-[-2em] z-10">
+              <button
+                className="bg-gray-900 rounded-full p-3 border-4 border-gray-800"
+                type="button"
+                onClick={() => {
+                  // need this else gonna freeze the browser if user spam click
+                  requestAnimationFrame(() => {
+                    setFromSelectedOption(toSelectedOption);
+                    setToSelectedOption(fromSelectedOption);
+                    setFromAmount((x) =>
+                      formattedResult.minimumReceiving === "0"
+                        ? x
+                        : formattedResult.minimumReceiving,
+                    );
+                  });
+                }}
+              >
+                <SwapIcon width="1.25em" height="1.25em" />
+              </button>
+            </div>
+            <fieldset className="flex flex-col gap-3 bg-black rounded-md p-6">
+              <legend className="float-left font-bold opacity-90">To</legend>
+              <Select
+                className="relative z-10"
+                value={toSelectedOption}
+                options={tokenOptions}
+                onChange={setToSelectedOption}
+              />
+              <Input
+                value={formattedResult.minimumReceiving}
+                fullWidth
+                disabled
+              />
+            </fieldset>
           </div>
-          <fieldset className="flex flex-col gap-3 bg-black rounded-md p-6">
-            <legend className="float-left font-bold opacity-90">To</legend>
-            <Select
-              className="relative z-10"
-              value={toSelectedOption}
-              options={tokenOptions}
-              onChange={setToSelectedOption}
-            />
-            <Input
-              value={formattedResult.minimumReceiving}
-              fullWidth
-              disabled
-            />
-          </fieldset>
-        </div>
-        <Button disabled={signer === undefined}>
-          {signer === undefined ? "Please Connect Sif Wallet" : "Swap"}
-        </Button>
-      </form>
-    </section>
+          <Button className="mt-8" disabled={signer === undefined}>
+            {signer === undefined ? "Please Connect Sif Wallet" : "Swap"}
+          </Button>
+        </form>
+      </section>
+    </div>
   );
 };
 
