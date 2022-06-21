680529033aa26dff89fabcbe5ab2a728188327c3
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-21 23:09:34 +1200

    feat: initial swap confirmation implementation

--- apps/dex/src/pages/swap.tsx
+++ apps/dex/src/pages/swap.tsx
@@ -2,21 +2,140 @@ import { Decimal } from "@cosmjs/math";
 import { Popover, Transition } from "@headlessui/react";
 import { runCatching } from "@sifchain/common";
 import {
+  ArrowDownIcon,
   ArrowLeftIcon,
   Button,
   ButtonGroup,
+  ButtonProps,
   Input,
+  Modal,
+  RacetrackSpinnerIcon,
   Select,
   SelectOption,
   SettingsIcon,
   SwapIcon,
 } from "@sifchain/ui";
-import { Fragment, useMemo, useState } from "react";
+import clsx from "clsx";
+import {
+  Fragment,
+  PropsWithChildren,
+  startTransition,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import AssetIcon from "~/compounds/AssetIcon";
 import { useSwapMutation } from "~/domains/clp";
 import { useTokenRegistryQuery } from "~/domains/tokenRegistry";
 import useSifnodeQuery from "~/hooks/useSifnodeQuery";
 import useSifSigner from "~/hooks/useSifSigner";
 import { useSifStargateClient } from "~/hooks/useSifStargateClient";
+
+type SwapConfirmationModalProps = {
+  title: string;
+  show: boolean;
+  onClose: () => unknown;
+  confirmationButtonProps: ButtonProps;
+  showDetail: boolean;
+  fromCoin: {
+    amount: string;
+    denom: string;
+  };
+  toCoin: {
+    amount: string;
+    minimumAmount: string;
+    denom: string;
+  };
+  liquidityProviderFee: string;
+  priceImpact: string;
+  slippage: string;
+};
+
+const ConfirmationLineItem = (
+  props: PropsWithChildren<{ className?: string }>,
+) => (
+  <li
+    className={clsx(
+      "flex justify-between align-middle px-4 py-3 rounded-lg",
+      props.className,
+    )}
+  >
+    {props.children}
+  </li>
+);
+
+const SwapConfirmationModal = (props: SwapConfirmationModalProps) => {
+  return (
+    <Modal title={props.title} isOpen={props.show} onClose={props.onClose}>
+      <ul>
+        <ConfirmationLineItem className="bg-black font-bold uppercase">
+          <div className="flex align-middle gap-1">
+            <AssetIcon
+              network="sifchain"
+              symbol={props.fromCoin.denom}
+              size="md"
+            />
+            {props.fromCoin.denom}
+          </div>
+          <span>{props.fromCoin.amount}</span>
+        </ConfirmationLineItem>
+        <div className="flex justify-center my-[-1.25em]">
+          <div className="bg-gray-900 rounded-full p-3 border-2 border-gray-800">
+            <ArrowDownIcon width="1em" height="1em" />
+          </div>
+        </div>
+        <Transition
+          show={props.showDetail}
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <ConfirmationLineItem>
+            <span>Swap result</span>
+            <div className="flex align-middle gap-1 font-bold">
+              {props.toCoin.amount}
+              <AssetIcon
+                network="sifchain"
+                symbol={props.toCoin.denom}
+                size="md"
+              />
+            </div>
+          </ConfirmationLineItem>
+          <ConfirmationLineItem>
+            <span>Liquidity provider fee</span>
+            <span>{props.liquidityProviderFee}</span>
+          </ConfirmationLineItem>
+          <ConfirmationLineItem>
+            <span>Price impact</span>
+            <span>{props.priceImpact}</span>
+          </ConfirmationLineItem>
+        </Transition>
+        <ConfirmationLineItem className="bg-black font-bold uppercase">
+          <div className="flex align-middle gap-1">
+            <AssetIcon network="sifchain" symbol="rowan" size="md" />
+            {props.toCoin.denom}
+          </div>
+          <span>{props.toCoin.amount}</span>
+        </ConfirmationLineItem>
+        <ConfirmationLineItem>
+          <span>Slippage</span>
+          <span>{props.slippage}</span>
+        </ConfirmationLineItem>
+        <ConfirmationLineItem>
+          <span>Minimum received</span>
+          <div className="flex align-middle gap-1 font-bold">
+            {props.toCoin.minimumAmount}
+            <AssetIcon network="sifchain" symbol="rowan" size="md" />
+          </div>
+        </ConfirmationLineItem>
+      </ul>
+      <Button className="w-full mt-8" {...props.confirmationButtonProps} />
+    </Modal>
+  );
+};
 
 const SwapPage = () => {
   const swapMutation = useSwapMutation();
@@ -34,6 +153,8 @@ const SwapPage = () => {
     body: "rowan",
   });
 
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+
   const fromDenom = fromSelectedOption.id;
   const toDenom = toSelectedOption.id;
 
@@ -47,7 +168,10 @@ const SwapPage = () => {
 
   const [fromAmount, setFromAmount] = useState("");
 
-  const commonOptions = { refetchInterval: 6000 };
+  const commonOptions = {
+    refetchInterval: 6000,
+    enabled: !isConfirmationModalOpen,
+  };
   const pmtpParamsQuery = useSifnodeQuery(
     "clp.getPmtpParams",
     [{}],
@@ -118,137 +242,230 @@ const SwapPage = () => {
     ],
   );
 
-  const formattedResult = useMemo(
+  const parsedSwapResult = useMemo(
     () => ({
       ...swapSimulationResult,
       rawReceiving: Decimal.fromAtomics(
         swapSimulationResult?.rawReceiving ?? "0",
         tokenRegistryQuery.indexedByIBCDenom[toDenom]?.decimals ?? 0,
-      ).toString(),
+      ),
       minimumReceiving: Decimal.fromAtomics(
         swapSimulationResult?.minimumReceiving ?? "0",
         tokenRegistryQuery.indexedByIBCDenom[toDenom]?.decimals ?? 0,
-      ).toString(),
+      ),
       liquidityProviderFee: Decimal.fromAtomics(
         swapSimulationResult?.liquidityProviderFee ?? "0",
         tokenRegistryQuery.indexedByIBCDenom[toDenom]?.decimals ?? 0,
-      ).toString(),
+      ),
     }),
     [swapSimulationResult, toDenom, tokenRegistryQuery.indexedByIBCDenom],
   );
 
+  useEffect(
+    () => {
+      // reset mutation state every time modal is closed
+      if (!isConfirmationModalOpen && swapMutation.status !== "idle") {
+        swapMutation.reset();
+      }
+    }, // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isConfirmationModalOpen],
+  );
+
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
-      <section className="flex-1 flex flex-col bg-gray-800 w-full h-full md:rounded-xl md:flex-initial md:w-auto md:h-auto p-6">
-        <header className="flex items-center justify-between pb-6">
-          <h2 className="text-2xl font-bold text-white">Swap</h2>
-          <Popover className="relative">
-            {({ open }) => (
-              <>
-                <Popover.Button>
-                  {open ? <ArrowLeftIcon /> : <SettingsIcon />}
-                </Popover.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition duration-100 ease-out"
-                  enterFrom="transform scale-95 opacity-0"
-                  enterTo="transform scale-100 opacity-100"
-                  leave="transition duration-75 ease-out"
-                  leaveFrom="transform scale-100 opacity-100"
-                  leaveTo="transform scale-95 opacity-0"
-                >
-                  <Popover.Panel className="absolute z-30 right-[-100%] bg-gray-700 rounded-lg border border-gray-600 p-4">
-                    <div className="flex flex-col">
-                      <legend className="float-left font-bold">Settings</legend>
-                      <div className="flex items-center">
-                        <label className="pr-6 text-sm opacity-90">
-                          Slippage
-                        </label>
-                        <ButtonGroup
-                          itemClassName="px-4"
-                          size="sm"
-                          gap={8}
-                          selectedIndex={selectedSlippageIndex}
-                          options={slippageOptions}
-                          onChange={setSelectedSlippageIndex}
-                        />
+    <>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <section className="flex-1 flex flex-col bg-gray-800 w-full h-full md:rounded-xl md:flex-initial md:w-auto md:h-auto p-6">
+          <header className="flex items-center justify-between pb-6">
+            <h2 className="text-2xl font-bold text-white">Swap</h2>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button>
+                    {open ? <ArrowLeftIcon /> : <SettingsIcon />}
+                  </Popover.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+                  >
+                    <Popover.Panel className="absolute z-30 right-[-100%] bg-gray-700 rounded-lg border border-gray-600 p-4">
+                      <div className="flex flex-col">
+                        <legend className="float-left font-bold">
+                          Settings
+                        </legend>
+                        <div className="flex items-center">
+                          <label className="pr-6 text-sm opacity-90">
+                            Slippage
+                          </label>
+                          <ButtonGroup
+                            itemClassName="px-4"
+                            size="sm"
+                            gap={8}
+                            selectedIndex={selectedSlippageIndex}
+                            options={slippageOptions}
+                            onChange={setSelectedSlippageIndex}
+                          />
+                        </div>
                       </div>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
-        </header>
-        <form
-          className="flex-1 flex flex-col justify-between"
-          onSubmit={(event) => {
-            event.preventDefault();
-            swapMutation.mutate({
-              fromDenom,
-              toDenom,
-              fromAmount: fromAmountDecimal?.atomics ?? "0",
-              minimumReceiving: swapSimulationResult?.minimumReceiving ?? "0",
-            });
-          }}
-        >
-          <div className="flex flex-col gap-3">
-            <fieldset className="flex flex-col gap-2 bg-black rounded-md p-6">
-              <legend className="float-left font-bold opacity-90">From</legend>
-              <Select
-                className="relative z-20"
-                value={fromSelectedOption}
-                options={tokenOptions}
-                onChange={setFromSelectedOption}
-              />
-              <Input
-                placeholder="Swap amount"
-                value={fromAmount}
-                onChange={(event) => setFromAmount(event.target.value)}
-                fullWidth
-              />
-            </fieldset>
-            <div className="flex justify-center align-middle my-[-2em] z-10">
-              <button
-                className="bg-gray-900 rounded-full p-3 border-4 border-gray-800"
-                type="button"
-                onClick={() => {
-                  // need this else gonna freeze the browser if user spam click
-                  requestAnimationFrame(() => {
-                    setFromSelectedOption(toSelectedOption);
-                    setToSelectedOption(fromSelectedOption);
-                    setFromAmount((x) =>
-                      formattedResult.minimumReceiving === "0"
-                        ? x
-                        : formattedResult.minimumReceiving,
-                    );
-                  });
-                }}
-              >
-                <SwapIcon width="1.25em" height="1.25em" />
-              </button>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </header>
+          <form
+            className="flex-1 flex flex-col justify-between"
+            onSubmit={(event) => {
+              event.preventDefault();
+              startTransition(() => setIsConfirmationModalOpen(true));
+            }}
+          >
+            <div className="flex flex-col gap-3">
+              <fieldset className="flex flex-col gap-2 bg-black rounded-md p-6 pb-10">
+                <legend className="float-left font-bold opacity-90">
+                  From
+                </legend>
+                <Select
+                  className="relative z-20"
+                  value={fromSelectedOption}
+                  options={tokenOptions}
+                  onChange={setFromSelectedOption}
+                />
+                <Input
+                  placeholder="Swap amount"
+                  value={fromAmount}
+                  onChange={(event) => setFromAmount(event.target.value)}
+                  fullWidth
+                />
+              </fieldset>
+              <div className="flex justify-center align-middle my-[-2em] z-10">
+                <button
+                  className="bg-gray-900 rounded-full p-3 border-4 border-gray-800"
+                  type="button"
+                  onClick={() => {
+                    // need this else gonna freeze the browser if user spam click
+                    requestAnimationFrame(() => {
+                      setFromSelectedOption(toSelectedOption);
+                      setToSelectedOption(fromSelectedOption);
+                      setFromAmount((x) =>
+                        parsedSwapResult.minimumReceiving.toFloatApproximation() ===
+                        0
+                          ? x
+                          : parsedSwapResult.minimumReceiving.toString(),
+                      );
+                    });
+                  }}
+                >
+                  <SwapIcon width="1.25em" height="1.25em" />
+                </button>
+              </div>
+              <fieldset className="flex flex-col gap-3 bg-black rounded-md p-6">
+                <legend className="float-left font-bold opacity-90">To</legend>
+                <Select
+                  className="relative z-10"
+                  value={toSelectedOption}
+                  options={tokenOptions}
+                  onChange={setToSelectedOption}
+                />
+                <Input
+                  value={parsedSwapResult.minimumReceiving.toString()}
+                  fullWidth
+                  disabled
+                />
+              </fieldset>
             </div>
-            <fieldset className="flex flex-col gap-3 bg-black rounded-md p-6">
-              <legend className="float-left font-bold opacity-90">To</legend>
-              <Select
-                className="relative z-10"
-                value={toSelectedOption}
-                options={tokenOptions}
-                onChange={setToSelectedOption}
-              />
-              <Input
-                value={formattedResult.minimumReceiving}
-                fullWidth
-                disabled
-              />
-            </fieldset>
-          </div>
-          <Button className="mt-8" disabled={signer === undefined}>
-            {signer === undefined ? "Please Connect Sif Wallet" : "Swap"}
-          </Button>
-        </form>
-      </section>
-    </div>
+            <Button className="mt-8" disabled={signer === undefined}>
+              {signer === undefined ? "Please Connect Sif Wallet" : "Swap"}
+            </Button>
+          </form>
+        </section>
+      </div>
+      <SwapConfirmationModal
+        title={(() => {
+          switch (swapMutation.status) {
+            case "idle":
+              return "Review swap";
+            case "loading":
+              return "Waiting for confirmation";
+            case "success":
+              return "Transaction submitted";
+            case "error":
+              return "Transaction failed";
+          }
+        })()}
+        show={isConfirmationModalOpen}
+        onClose={() => startTransition(() => setIsConfirmationModalOpen(false))}
+        showDetail={swapMutation.isIdle}
+        confirmationButtonProps={{
+          children: (() => {
+            switch (swapMutation.status) {
+              case "idle":
+                return "Confirm";
+              case "loading":
+                return <RacetrackSpinnerIcon />;
+              case "success":
+                return "Close";
+              case "error":
+                return "Close";
+            }
+          })(),
+          disabled: swapMutation.isLoading,
+          onClick: () => {
+            switch (swapMutation.status) {
+              case "idle":
+                swapMutation.mutate({
+                  fromDenom,
+                  toDenom,
+                  fromAmount: fromAmountDecimal?.atomics ?? "0",
+                  minimumReceiving:
+                    swapSimulationResult?.minimumReceiving ?? "0",
+                });
+                break;
+              case "loading":
+                return;
+              default:
+                setIsConfirmationModalOpen(false);
+            }
+          },
+        }}
+        fromCoin={{
+          denom:
+            tokenRegistryQuery.indexedByIBCDenom[fromDenom]?.displaySymbol ??
+            "",
+          amount: (
+            fromAmountDecimal?.toFloatApproximation() ?? 0
+          ).toLocaleString(undefined, { maximumFractionDigits: 6 }),
+        }}
+        toCoin={{
+          denom:
+            tokenRegistryQuery.indexedByIBCDenom[toDenom]?.displaySymbol ?? "",
+          amount: parsedSwapResult.rawReceiving
+            .toFloatApproximation()
+            .toLocaleString(undefined, { maximumFractionDigits: 6 }),
+          minimumAmount: parsedSwapResult.minimumReceiving
+            .toFloatApproximation()
+            .toLocaleString(undefined, { maximumFractionDigits: 6 }),
+        }}
+        liquidityProviderFee={parsedSwapResult.liquidityProviderFee
+          .toFloatApproximation()
+          .toLocaleString(undefined, { maximumFractionDigits: 6 })}
+        priceImpact={(parsedSwapResult.priceImpact ?? 0).toLocaleString(
+          undefined,
+          {
+            style: "percent",
+            maximumFractionDigits: 2,
+          },
+        )}
+        slippage={(slippage ?? 0).toLocaleString(undefined, {
+          style: "percent",
+          maximumFractionDigits: 2,
+        })}
+      />
+    </>
   );
 };
 
