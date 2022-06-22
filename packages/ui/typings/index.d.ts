bbb0c1f5be629ac1066cd16c5ff277d2ce17b0f3
Alan <alanrsoars@gmail.com>
2022-06-22 16:27:17 +1200

    chore: add module typings for walletconnect-qrcode-modal

---
+++ packages/ui/typings/index.d.ts
@@ -0,0 +1,7 @@
+declare module "walletconnect-qrcode-modal" {
+  interface IWallectConnectModal {
+    open(uri: string): void;
+  }
+  const WallectConnectModal: IWallectConnectModal;
+  export default IWallectConnectModal;
+}
