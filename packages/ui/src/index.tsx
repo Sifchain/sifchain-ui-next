daa0a329bb7f7ab2b1b7880dbaefc4eb9a5d3f6f
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-22 18:21:36 +1200

    fix: missing reference to d.ts file

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../typings/index.d.ts" />
+
 export * from "./hooks";
 export * from "./compounds";
 export * from "./components";
