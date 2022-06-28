aade13959cbbc374c61fc998a24e5db4f26fbe96
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-06-28 21:52:56 +1200

    refactor: balances hook

--- apps/dex/src/domains/bank/hooks/balances.ts
+++ apps/dex/src/domains/bank/hooks/balances.ts
@@ -1,7 +1,5 @@
 import { Decimal } from "@cosmjs/math";
-import type { IAsset } from "@sifchain/common";
 import { useSigner } from "@sifchain/cosmos-connect";
-import BigNumber from "bignumber.js";
 import { indexBy, prop } from "rambda";
 import { useQuery } from "react-query";
 import { useDexEnvironment } from "~/domains/core/envs";
@@ -27,6 +25,9 @@ export const useAllBalances = () => {
 
       return balances?.map((x) => ({
         ...x,
+        symbol: indexedByIBCDenom[x.denom]?.symbol,
+        displaySymbol: indexedByIBCDenom[x.denom]?.displaySymbol,
+        network: indexedByIBCDenom[x.denom]?.network,
         amount: Decimal.fromAtomics(
           x.amount,
           indexedByIBCDenom[x.denom]?.decimals ?? 0,
@@ -48,48 +49,4 @@ export const useAllBalances = () => {
         ? undefined
         : indexBy(prop("denom"), baseQuery.data),
   };
-};
-
-export const useAllDisplayBalances = () => {
-  const { data: balances } = useAllBalances();
-  const {
-    data: assets,
-    indexedBySymbol,
-    indexedByDisplaySymbol,
-    indexedByIBCDenom,
-  } = useTokenRegistryQuery();
-
-  return useQuery(
-    "all-display-balances",
-    async () => {
-      const records = balances
-        ?.map((x) => {
-          const tokenRecord =
-            indexedBySymbol[x.denom.toLowerCase()] ||
-            indexedByIBCDenom[x.denom] ||
-            indexedByIBCDenom[x.denom.toLowerCase()] ||
-            indexedByIBCDenom[x.denom.slice(1).toLowerCase()] ||
-            indexedByDisplaySymbol[x.denom.toLowerCase()] ||
-            indexedBySymbol[x.denom.slice(1).toLowerCase()];
-
-          if (!tokenRecord) {
-            console.warn("tokenRecord not found", x.denom);
-            return;
-          }
-
-          return [tokenRecord, x] as const;
-        })
-        .filter(Boolean) as [IAsset, { amount: Decimal; denom: string }][];
-
-      return records?.map(([tokenRecord, x]) => ({
-        denom: tokenRecord?.displaySymbol ?? tokenRecord?.symbol ?? x.denom,
-        amount: new BigNumber(x.amount.atomics).shiftedBy(
-          -(tokenRecord?.decimals ?? 0),
-        ),
-      }));
-    },
-    {
-      enabled: balances !== undefined && assets !== undefined,
-    },
-  );
 };
