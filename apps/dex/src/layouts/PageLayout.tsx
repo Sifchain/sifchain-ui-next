2b3d21bfb1a1a497387bae7dd3ead09d7bd82fd6
Alan <alanrsoars@gmail.com>
2022-06-29 00:11:44 +1200

    feat: wip page layout

--- apps/dex/src/layouts/PageLayout.tsx
+++ apps/dex/src/layouts/PageLayout.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeftIcon } from "@heroicons/react/outline";
+import { SurfaceB } from "@sifchain/ui";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import type { FC, PropsWithChildren, ReactNode } from "react";
@@ -19,21 +20,23 @@ const PageLayout: FC<PropsWithChildren<Props>> = (props) => {
           <title>Sichain Dex - {props.title ?? props.heading}</title>
         </Head>
       )}
-      <header className="md:flex items-center p-2 bg-slate-200/40 dark:bg-gray-900/80 gap-2">
-        {props.heading && (
-          <nav className="flex items-center gap-2">
-            {props.withBackNavigation && (
-              <BackButton onClick={() => router.back()} />
-            )}
-            <span className="py-0.5 px-2 rounded text-gray-900 dark:text-gray-300 before:content-['/_']">
-              {props.heading}
-            </span>
-          </nav>
-        )}
-      </header>
-      <div className="p-6 overflow-y-scroll flex-1 max-w-6xl mx-auto w-full">
-        {props.children}
-      </div>
+      <SurfaceB className="rounded-2xl flex-1 max-w-6xl mx-auto w-full my-8">
+        <header className="md:flex items-center p-2 gap-2">
+          {props.heading && (
+            <nav className="flex items-center gap-2">
+              {props.withBackNavigation && (
+                <BackButton onClick={() => router.back()} />
+              )}
+              <span className="py-0.5 px-2 rounded text-gray-300 before:content-['/_']">
+                {props.heading}
+              </span>
+            </nav>
+          )}
+        </header>
+        <div className="p-6 max-h-[calc(100vh-200px)] overflow-y-scroll">
+          {props.children}
+        </div>
+      </SurfaceB>
     </>
   );
 };
