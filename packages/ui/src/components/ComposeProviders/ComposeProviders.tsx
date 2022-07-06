21a32dcf7882bc64422ca8d53d1701355fc1caa6
Alan <alanrsoars@gmail.com>
2022-07-07 11:42:22 +1200

    feat: introduce utility component ComposeProviders

---
+++ packages/ui/src/components/ComposeProviders/ComposeProviders.tsx
@@ -0,0 +1,19 @@
+import type { FC, PropsWithChildren } from "react";
+
+export type ComposeProvidersProps = PropsWithChildren<{
+  providers: FC<PropsWithChildren>[];
+}>;
+
+export const ComposeProviders: FC<ComposeProvidersProps> = ({
+  providers,
+  children,
+}) => (
+  <>
+    {providers.reduce(
+      (acc, Provider) => (
+        <Provider>{acc}</Provider>
+      ),
+      children,
+    )}
+  </>
+);
