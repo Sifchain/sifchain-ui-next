b04942a3bb8c65a4b3951f25e64a6aa9f8c8dc43
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-07-12 15:52:55 +1200

    feat: add router sync between client x server for Margin/Portifilio tabs

--- apps/dex/src/pages/margin/index.tsx
+++ apps/dex/src/pages/margin/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
+import type { ParsedUrlQuery } from "querystring";
 
 import { TabsWithSuspense, TabsWithSuspenseProps } from "@sifchain/ui";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import Link from "next/link";
@@ -23,21 +25,48 @@ const TAB_ITEMS: TabsWithSuspenseProps["items"] = [
 
 const Margin: NextPage = () => {
   const router = useRouter();
-  const activeTab = (router.query["tab"] as string) || DEFAULT_TAB_ITEM;
-  return (
-    <PageLayout heading="Margin">
+  const [querystring, setQuerystring] = useState<ParsedUrlQuery | null>(null);
+
+  /**
+   * If a page does not have data fetching methods, router.query will be an empty object
+   * on the page's first load, when the page gets pre-generated on the server.
+   * https://nextjs.org/docs/api-reference/next/router#router-object
+   *
+   * Because of that, the nested tab flickers. For example:
+   *    /margin?tab=portifolio&option=history
+   *
+   * The above URL will NOT render the correct "History" option at first load
+   * It will render "Open positions" (hardcoded default) and then flicks to "History"
+   * This is not an ideal UX; hence, we need the sync below with router.isReady
+   */
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+    setQuerystring(router.query);
+  }, [router.isReady, router.query]);
+
+  let content = <div className="bg-gray-850 p-10 text-center">Loading...</div>;
+
+  if (querystring !== null) {
+    const activeTab = (querystring["tab"] as string) || DEFAULT_TAB_ITEM;
+    content = (
       <TabsWithSuspense
         activeTab={activeTab}
         items={TAB_ITEMS}
-        loadingFallback="Loading..."
+        loadingFallback={
+          <div className="bg-gray-850 p-10 text-center">Loading...</div>
+        }
         renderItem={(title, slug) => (
           <Link href={{ query: { tab: slug } }}>
             <a className="flex py-2">{title}</a>
           </Link>
         )}
       />
-    </PageLayout>
-  );
+    );
+  }
+
+  return <PageLayout heading="Margin">{content}</PageLayout>;
 };
 
 export default Margin;
