2d78a2403afebd977e2a9badbaf1267f6ca085df
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-07-12 15:13:34 +1200

    feat: add Margin>OpenPositions and Maring>History dynamic compounds

---
+++ apps/dex/src/compounds/Margin/History.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from "next";
+
+import { Slug } from "~/components/Slug";
+
+const HISTORY_HEADER_ITEMS = [
+  "Date",
+  "Pool",
+  "Side",
+  "Amount",
+  "Leverage",
+  "Realized P&L",
+];
+
+const Trade: NextPage = () => {
+  return (
+    <div className="overflow-x">
+      <table className="table-auto overflow-scroll w-full text-left text-sm">
+        <thead>
+          <tr className="text-gray-400">
+            {HISTORY_HEADER_ITEMS.map((title) => {
+              return (
+                <th key={title} className="font-normal px-4 py-3">
+                  {title}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody className="bg-gray-850">
+          <tr>
+            <td className="px-4 py-3">07-05 11:23:51 AM</td>
+            <td className="px-4 py-3">ETH / ROWAN</td>
+            <td className="px-4 py-3">
+              <Slug color="green" title="Long" />
+            </td>
+            <td className="px-4 py-3">5.00000000</td>
+            <td>2x</td>
+            <td className="px-4 py-3">
+              <Slug color="green" title="0.002 (0.0%)" />
+            </td>
+          </tr>
+          <tr>
+            <td className="px-4 py-3">07-05 11:23:51 AM</td>
+            <td className="px-4 py-3">ETH / ROWAN</td>
+            <td className="px-4 py-3">
+              <Slug color="red" title="Short" />
+            </td>
+            <td className="px-4 py-3">5.00000000</td>
+            <td>2x</td>
+            <td className="px-4 py-3">
+              <Slug color="green" title="0.002 (0.0%)" />
+            </td>
+          </tr>
+          <tr>
+            <td
+              colSpan={HISTORY_HEADER_ITEMS.length}
+              className="text-gray-400 text-center p-20"
+            >
+              You have no open positions.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Trade;
--- apps/dex/src/compounds/Margin/Portifolio.tsx
+++ apps/dex/src/compounds/Margin/Portifolio.tsx
@@ -1,7 +1,59 @@
 import type { NextPage } from "next";
 
+import { Suspense } from "react";
+import { useRouter } from "next/router";
+import clsx from "clsx";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+const DEFAULT_OPTION_ITEM = "open-positions";
+const OPTIONS_ITEMS = [
+  {
+    title: "Open Positions",
+    slug: "open-positions",
+    content: dynamic(() => import("~/compounds/Margin/OpenPositions")),
+  },
+  {
+    title: "History",
+    slug: "history",
+    content: dynamic(() => import("~/compounds/Margin/History")),
+  },
+];
+
 const Portifolio: NextPage = () => {
-  return <h1>Portifolio Page</h1>;
+  const router = useRouter();
+  const activeOption =
+    (router.query["option"] as string) || DEFAULT_OPTION_ITEM;
+  const currentTab = OPTIONS_ITEMS.find((item) => item.slug === activeOption);
+  const TabContent = currentTab?.content || null;
+  return (
+    <section className="mt-4 rounded bg-gray-800">
+      <ul className="flex flex-row">
+        {OPTIONS_ITEMS.map(({ title, slug }) => {
+          const isTabActive = currentTab?.slug === slug;
+          return (
+            <li key={slug}>
+              <Link href={{ query: { ...router.query, option: slug } }}>
+                <a
+                  className={clsx(
+                    "flex mx-4 py-3",
+                    isTabActive ? "text-white border-current" : "text-gray-400",
+                  )}
+                >
+                  {title}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      {TabContent && (
+        <Suspense fallback="Loading...">
+          <TabContent />
+        </Suspense>
+      )}
+    </section>
+  );
 };
 
 export default Portifolio;
