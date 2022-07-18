1efa36bc5205c464bc96b7f6b60b028551e82bb8
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-07-18 15:41:07 +1200

    feat: update Trade Pool overview layout to match Figma

--- apps/dex/src/compounds/Margin/Trade.tsx
+++ apps/dex/src/compounds/Margin/Trade.tsx
@@ -4,49 +4,65 @@ import { Button, TwinRadioGroup } from "@sifchain/ui";
 
 import TokenSelector from "~/compounds/TokenSelector";
 import OpenPositionsTable from "~/compounds/Margin/OpenPositions";
+import { useEnhancedPoolsQuery } from "~/domains/clp";
+
+function PoolSelector() {
+  const pools = useEnhancedPoolsQuery();
+  console.log(pools);
+  return (
+    <select className="text-xs bg-gray-700 rounded border-0 w-full">
+      <option>ETH / ROWAN</option>
+    </select>
+  );
+}
 
 const Trade: NextPage = () => {
   return (
     <>
-      <section className="bg-gray-800 border border-gold-800 rounded p-4 mt-4 grid grid-cols-7 gap-x-5 justify-items-end text-xs">
-        <ul className="col-span-2 w-full flex flex-col">
-          <li className="text-gray-300 pb-2">Pool Overview</li>
-          <li className="flex-1 flex items-center">
-            <select className="flex-1 text-xs bg-gray-700 rounded border-0">
-              <option>ETH / ROWAN</option>
-            </select>
+      <section className="bg-gray-800 border border-gold-800 rounded mt-4 text-xs">
+        <ul className="grid grid-cols-7 gap-5">
+          <li className="col-span-2 pl-4 py-4">
+            <PoolSelector />
           </li>
-        </ul>
-        <ul className="flex flex-col">
-          <li className="text-gray-300 pb-2">Pool</li>
-          <li className="flex-1 flex items-center font-semibold">
-            $100,000,000
+          <li className="py-4">
+            <div className="flex flex-col">
+              <span className="text-gray-300">Pool TVL (% change)</span>
+              <span className="font-semibold text-sm">$100,000,000</span>
+            </div>
           </li>
-        </ul>
-        <ul className="flex flex-col">
-          <li className="text-gray-300 pb-2">ETH/ROWAN Volume</li>
-          <li className="flex-1 flex items-center font-semibold">$2,000,000</li>
-        </ul>
-        <ul className="flex flex-col">
-          <li className="text-gray-300 pb-2">ETH Price</li>
-          <li className="flex-1 flex items-center font-semibold">
-            <span className="mr-2">$1,000</span>
-            <span className="text-red-400">(-1.3%)</span>
+          <li className="py-4">
+            <div className="flex flex-col">
+              <span className="text-gray-300">Pool Volume (% change)</span>
+              <span className="font-semibold text-sm">$2,000,000</span>
+            </div>
           </li>
-        </ul>
-        <ul className="flex flex-col">
-          <li className="text-gray-300 pb-2">ROWAN Price</li>
-          <li className="flex-1 flex items-center font-semibold">
-            <span className="mr-2">$.006</span>
-            <span className="text-green-400">(+2.3%)</span>
+          <li className="py-4">
+            <div className="flex flex-col">
+              <span className="text-gray-300">ETH Price (% change)</span>
+              <span className="font-semibold text-sm">
+                <span className="mr-1">$1,000</span>
+                <span className="text-red-400">(-2.8%)</span>
+              </span>
+            </div>
           </li>
-        </ul>
-        <ul className="flex flex-col">
-          <li className="text-gray-300 pb-2">Pool Health</li>
-          <li className="flex-1 flex items-center font-semibold">&ndash;</li>
+          <li className="py-4">
+            <div className="flex flex-col">
+              <span className="text-gray-300">ROWAN price (% change)</span>
+              <span className="font-semibold text-sm">
+                <span className="mr-1">$.006</span>
+                <span className="text-red-400">(-1.3%)</span>
+              </span>
+            </div>
+          </li>
+          <li className="py-4">
+            <div className="flex flex-col">
+              <span className="text-gray-300">Pool Health</span>
+              <span className="font-semibold text-sm">&ndash;</span>
+            </div>
+          </li>
         </ul>
       </section>
-      <section className="mt-4 text-xs grid grid-cols-7 gap-x-2">
+      <section className="mt-4 text-xs grid grid-cols-7 gap-x-5">
         <aside className="bg-gray-800 border border-gold-800 rounded col-span-2 flex flex-col">
           <ul className="border-b border-gold-800 flex flex-col gap-4 p-4">
             <li>
