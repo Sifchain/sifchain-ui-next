644ad74bb51fcfdda4ba7781b42e9d58f1af2c63
Alan <alanrsoars@gmail.com>
2022-07-19 14:38:39 +1200

    refactor: allow passing custom decimal precision

--- packages/ui/src/utils/intl.ts
+++ packages/ui/src/utils/intl.ts
@@ -1,16 +1,16 @@
-export const currencyFormatter = Intl.NumberFormat(undefined, {
-  style: "currency",
-  currency: "USD",
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
-
 /**
  * Formats a number as currency.
  *
  * @param number - number to format
  * @returns formatted currency
  */
-export function formatNumberAsCurrency(number: number): string {
-  return currencyFormatter.format(number);
+export function formatNumberAsCurrency(number: number, decimals = 2): string {
+  const formatter = Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  });
+
+  return formatter.format(number);
 }
