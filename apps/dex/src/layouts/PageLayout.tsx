c0d643576baf6396cc4c3cd5125498bc4c98c2a5
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-07-19 17:45:33 +1200

    revert: remove classname from PageLayout, Trade page is using Balances styles

--- apps/dex/src/layouts/PageLayout.tsx
+++ apps/dex/src/layouts/PageLayout.tsx
@@ -3,10 +3,8 @@ import { SurfaceB } from "@sifchain/ui";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import type { FC, PropsWithChildren, ReactNode } from "react";
-import clsx from "clsx";
 
 type Props = {
-  className?: string;
   heading?: ReactNode;
   withBackNavigation?: boolean;
   title?: string;
@@ -22,12 +20,7 @@ const PageLayout: FC<PropsWithChildren<Props>> = (props) => {
           <title>Sichain Dex - {props.title ?? props.heading}</title>
         </Head>
       )}
-      <SurfaceB
-        className={clsx(
-          props.className || "max-w-6xl",
-          "rounded-2xl flex-1 mx-auto w-full my-8",
-        )}
-      >
+      <SurfaceB className="rounded-2xl flex-1 max-w-6xl mx-auto w-full my-8">
         <header className="md:flex items-center p-2 gap-2">
           {props.heading && (
             <nav className="flex items-center gap-2">
