7044e302651ac84b9b599053e79d8b7df597f926
Alan <alanrsoars@gmail.com>
2022-07-19 15:20:03 +1200

    chore: add AsyncImage story

--- packages/ui/src/components/AsyncImage/AsyncImage.stories.tsx
+++ packages/ui/src/components/AsyncImage/AsyncImage.stories.tsx
@@ -1,6 +1,7 @@
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { AsyncImage } from ".";
+import { RacetrackSpinnerIcon } from "../icons";
 
 export default {
   title: "components/AsyncImage",
@@ -13,4 +14,14 @@ const Template: ComponentStory<typeof AsyncImage> = (args) => {
 
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  src: "https://picsum.photos/300/300",
+  placeholder: (
+    <figure className="grid place-items-center">
+      <RacetrackSpinnerIcon />
+    </figure>
+  ),
+  height: 300,
+  width: 300,
+  className: "ring ring-gray-400 rounded shadow-lg shadow-black/70",
+};
