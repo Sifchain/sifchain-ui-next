95b71d06f9974027fb5ada8b5a8167d6b322c86c
Alan <alanrsoars@gmail.com>
2022-07-27 15:18:42 +1200

    style: allow fixed width tokenSelector by passin max-w-[x]

--- packages/ui/src/compounds/TokenSelector/TokenSelector.stories.tsx
+++ packages/ui/src/compounds/TokenSelector/TokenSelector.stories.tsx
@@ -21,56 +21,53 @@ export const FixedWidthParent: ComponentStory<typeof TokenSelector> = (
   args,
 ) => {
   return (
-    <div>
+    <div className="text-gray-200">
       <ol className="list-decimal pl-4 mb-4">
         <li>TokenSelctor respects the parent width</li>
         <li>Adds text ellipsis when overflow</li>
         <li>Do not break text in new line</li>
         <li>Label is optional</li>
       </ol>
-      <div style={{ width: 128 }}>
-        <TokenSelector {...args} />
-      </div>
+      <TokenSelector {...args} />
     </div>
   );
 };
 FixedWidthParent.args = {
   modalTitle: "From",
   tokens: [...tokens],
+  buttonClassName: "max-w-[140px]",
 };
 
 export const SizeAndButtonCustomization: ComponentStory<
   typeof TokenSelector
 > = (args) => {
   return (
-    <div>
+    <div className="text-gray-200">
       <ol className="list-decimal pl-4 mb-4">
         <li>You can add extra classes to style the button container</li>
         <li>Change the size of icons and text via `size` property</li>
       </ol>
-      <div style={{ width: 128 }}>
-        <TokenSelector {...args} />
-      </div>
+
+      <TokenSelector {...args} />
     </div>
   );
 };
+
 SizeAndButtonCustomization.args = {
   modalTitle: "From",
   tokens: [...tokens],
   size: "xs",
-  buttonClassName: "border-none rounded-none",
+  buttonClassName: "max-w-[120px]",
 };
 
 export const Readonly: ComponentStory<typeof TokenSelector> = (args) => {
   return (
-    <div>
+    <div className="text-gray-200">
       <ol className="list-decimal pl-4 mb-4">
         <li>You use `readonly` option to disable interactivity</li>
         <li>The icon will change to a lock, indicating you can't change it</li>
       </ol>
-      <div style={{ width: 256 }}>
-        <TokenSelector {...args} />
-      </div>
+      <TokenSelector {...args} />
     </div>
   );
 };
@@ -78,4 +75,5 @@ Readonly.args = {
   modalTitle: "From",
   tokens: [...tokens],
   readonly: true,
+  buttonClassName: "max-w-[255px]",
 };
