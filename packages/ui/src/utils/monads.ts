72c631e99b5c4e52637d082dc019ea9042ab16cb
Alan <alanrsoars@gmail.com>
2022-07-28 13:59:34 +1200

    refactor: rollback to ts private field

--- packages/ui/src/utils/monads.ts
+++ packages/ui/src/utils/monads.ts
@@ -3,39 +3,43 @@
  * loosely based on https://github.com/patrickmichalina/typescript-monads/blob/master/src/maybe/maybe.ts
  */
 export class Maybe<T> {
-  constructor(private readonly value?: T | null) {}
+  private _value: T | undefined | null;
 
-  public static of<T>(value: T) {
+  constructor(value?: T | null) {
+    this._value = value;
+  }
+
+  static of<T>(value: T) {
     return new Maybe(value);
   }
 
-  public valueOr<U>(defaultValue: U): T | U {
+  valueOr<U>(defaultValue: U): T | U {
     if (this.isSome) {
-      return this.value as NonNullable<T>;
+      return this._value as NonNullable<T>;
     }
 
     return defaultValue;
   }
 
-  public get isSome() {
-    return this.value !== undefined;
+  get isSome() {
+    return this._value !== undefined && this._value !== null;
   }
 
-  public get isNone() {
-    return this.value === null || this.value === undefined;
+  get isNone() {
+    return this._value === null || this._value === undefined;
   }
 
-  public map<U>(fn: (value: NonNullable<T>) => U) {
+  map<U>(fn: (value: NonNullable<T>) => U) {
     if (this.isSome) {
-      return Maybe.of(fn(this.value as NonNullable<T>));
+      return Maybe.of(fn(this._value as NonNullable<T>));
     }
 
     return new Maybe<U>(undefined);
   }
 
-  public mapOr<U>(defaultValue: U, fn: (value: NonNullable<T>) => U): U {
+  mapOr<U>(defaultValue: U, fn: (value: NonNullable<T>) => U): U {
     if (this.isSome) {
-      return fn(this.value as NonNullable<T>);
+      return fn(this._value as NonNullable<T>);
     }
 
     return defaultValue;
