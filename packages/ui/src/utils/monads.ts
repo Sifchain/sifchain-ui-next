c196780c74f79608a877700a428bd38e9c566eb8
Alan <alanrsoars@gmail.com>
2022-07-28 12:17:31 +1200

    feat: introduce Maybe monad

---
+++ packages/ui/src/utils/monads.ts
@@ -0,0 +1,43 @@
+/**
+ * Minimal implementation of a Maybe monad
+ * loosely based on https://github.com/patrickmichalina/typescript-monads/blob/master/src/maybe/maybe.ts
+ */
+export class Maybe<T> {
+  constructor(private readonly value?: T | null) {}
+
+  public static of<T>(value: T) {
+    return new Maybe(value);
+  }
+
+  public valueOr<U>(defaultValue: U): T | U {
+    if (this.isSome) {
+      return this.value as NonNullable<T>;
+    }
+
+    return defaultValue;
+  }
+
+  public get isSome() {
+    return this.value !== undefined;
+  }
+
+  public get isNone() {
+    return this.value === null || this.value === undefined;
+  }
+
+  public map<U>(fn: (value: NonNullable<T>) => U) {
+    if (this.isSome) {
+      return Maybe.of(fn(this.value as NonNullable<T>));
+    }
+
+    return new Maybe<U>(undefined);
+  }
+
+  public mapOr<U>(defaultValue: U, fn: (value: T) => U): U {
+    if (this.isSome) {
+      return fn(this.value as NonNullable<T>);
+    }
+
+    return defaultValue;
+  }
+}
