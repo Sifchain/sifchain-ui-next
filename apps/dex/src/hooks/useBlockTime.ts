bb58843d2e8fa586669356e68b1e05c6dd04fd6b
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-08-05 16:59:13 +1200

    feat: add block time utilities

---
+++ apps/dex/src/hooks/useBlockTime.ts
@@ -0,0 +1,28 @@
+import { invariant } from "@sifchain/ui";
+import { differenceInMilliseconds, parseISO } from "date-fns";
+import { useQuery } from "react-query";
+import { useSifStargateClient } from "./useSifStargateClient";
+
+export const useBlockTimeQuery = () => {
+  const { data: sifchainClients } = useSifStargateClient();
+
+  return useQuery(
+    "current-block-time",
+    async () => {
+      invariant(sifchainClients !== undefined, "sifchainClients is undefined");
+
+      const currentHeight = await sifchainClients.getHeight();
+
+      const currentBlock = await sifchainClients.getBlock(currentHeight);
+      const prevBlock = await sifchainClients.getBlock(currentHeight - 1);
+
+      return differenceInMilliseconds(
+        parseISO(currentBlock.header.time),
+        parseISO(prevBlock.header.time),
+      );
+    },
+    {
+      enabled: sifchainClients !== undefined,
+    },
+  );
+};
