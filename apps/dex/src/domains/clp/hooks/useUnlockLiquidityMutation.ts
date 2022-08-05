8b1ca2ec0bfe0484ab5c1684496180e3ea4fb394
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-08-05 17:05:39 +1200

    feat: add unlock toasts & queries invalidation

--- apps/dex/src/domains/clp/hooks/useUnlockLiquidityMutation.ts
+++ apps/dex/src/domains/clp/hooks/useUnlockLiquidityMutation.ts
@@ -1,10 +1,17 @@
+import { isDeliverTxFailure, isDeliverTxSuccess } from "@cosmjs/stargate";
 import { DEFAULT_FEE } from "@sifchain/stargate";
-import { invariant } from "@sifchain/ui";
-import { useMutation } from "react-query";
+import { invariant, toast } from "@sifchain/ui";
+import { isNil } from "rambda";
+import { useMutation, useQueryClient } from "react-query";
 import useSifSigner from "~/hooks/useSifSigner";
 import { useSifSigningStargateClient } from "~/hooks/useSifStargateClient";
+import {
+  LIQUIDITY_PROVIDERS_QUERY_KEY,
+  LIQUIDITY_PROVIDER_QUERY_KEY,
+} from "./liquidityProvider";
 
 const useUnlockLiquidityMutation = () => {
+  const queryClient = useQueryClient();
   const { signer } = useSifSigner();
   const { data: stargateClient } = useSifSigningStargateClient();
 
@@ -29,6 +36,31 @@ const useUnlockLiquidityMutation = () => {
         ],
         DEFAULT_FEE,
       );
+    },
+    {
+      onMutate: () => {
+        toast.info("Unbond liquidity inprogress");
+      },
+      onSettled: (data, error) => {
+        if (!isNil(error)) {
+          if (error instanceof Error || "message" in (error as Error)) {
+            toast.error((error as Error).message);
+          } else {
+            toast.error("Failed to unbond liquidity");
+          }
+          return;
+        }
+
+        if (data === undefined) return;
+
+        if (Boolean(error) || isDeliverTxFailure(data)) {
+          toast.error(data?.rawLog ?? "Failed to unbond liquidity");
+        } else if (data !== undefined && isDeliverTxSuccess(data)) {
+          toast.success(`Successfully unbonded liquidity`);
+          queryClient.invalidateQueries(LIQUIDITY_PROVIDER_QUERY_KEY);
+          queryClient.invalidateQueries(LIQUIDITY_PROVIDERS_QUERY_KEY);
+        }
+      },
     },
   );
 
