e3c418d0b758430635c0f135a3350d1ef113f687
Alan Soares <alanrsoars@gmail.com>
2022-08-10 16:49:39 +1200

    feat: split mutations into to their modules, with toasts to track tx lifecycle

--- apps/dex/src/domains/margin/hooks/useCloseMTPMutation.ts
+++ apps/dex/src/domains/margin/hooks/useCloseMTPMutation.ts
@@ -1,25 +1,25 @@
 import type * as MarginTX from "@sifchain/proto-types/sifnode/margin/v1/tx";
 import { DEFAULT_FEE } from "@sifchain/stargate";
 import { invariant, toast } from "@sifchain/ui";
-import { isError, useMutation } from "react-query";
+import { useMutation, isError } from "react-query";
 
 import { useSifSignerAddress } from "~/hooks/useSifSigner";
 import { useSifSigningStargateClient } from "~/hooks/useSifStargateClient";
 
-export type OpenMTPVariables = Omit<MarginTX.MsgOpen, "signer">;
+export type CloseMTPVariables = Omit<MarginTX.MsgClose, "signer">;
 
-export function useOpenMTPMutation() {
+export function useCloseMTPMutation() {
   const { data: signerAddress } = useSifSignerAddress();
   const { data: signingStargateClient } = useSifSigningStargateClient();
 
-  async function mutation(variables: OpenMTPVariables) {
+  async function mutation(variables: CloseMTPVariables) {
     invariant(signerAddress !== undefined, "Sif signer is not defined");
 
-    return signingStargateClient?.signAndBroadcast(
+    return await signingStargateClient?.signAndBroadcast(
       signerAddress,
       [
         {
-          typeUrl: "/sifnode.margin.v1.MsgOpen",
+          typeUrl: "/sifnode.margin.v1.MsgClose",
           value: {
             signer: signerAddress,
             ...variables,
@@ -32,16 +32,16 @@ export function useOpenMTPMutation() {
 
   return useMutation(mutation, {
     onMutate() {
-      toast.info("Open MTP inprogress");
+      toast.info("Close MTP inprogress");
     },
     onSuccess() {
-      toast.success("Open MTP success");
+      toast.success("Close MTP success");
     },
     onError(error) {
       if (isError(error)) {
         toast.error(error.message);
       } else {
-        toast.error("Failed to open MTP");
+        toast.error("Failed to close MTP");
       }
     },
   });
