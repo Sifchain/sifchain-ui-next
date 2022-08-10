f3897389c44707ee769567b66064580a415b07b4
Alan Soares <alanrsoars@gmail.com>
2022-08-10 16:14:48 +1200

    feat: expose margin module

--- packages/stargate/src/messages.ts
+++ packages/stargate/src/messages.ts
@@ -11,13 +11,16 @@ import type {
 import type * as clpTx from "@sifchain/proto-types/sifnode/clp/v1/tx";
 import type * as dispensationTx from "@sifchain/proto-types/sifnode/dispensation/v1/tx";
 import type * as ethBridgeTx from "@sifchain/proto-types/sifnode/ethbridge/v1/tx";
+import type * as marginTx from "@sifchain/proto-types/sifnode/margin/v1/tx";
 import type * as tokenRegistryTx from "@sifchain/proto-types/sifnode/tokenregistry/v1/tx";
+
 import type { EncodeObjectRecord } from "./types";
 
 export type SifEncodeObjectRecord = EncodeObjectRecord<typeof clpTx> &
   EncodeObjectRecord<typeof dispensationTx> &
   EncodeObjectRecord<typeof ethBridgeTx> &
-  EncodeObjectRecord<typeof tokenRegistryTx>;
+  EncodeObjectRecord<typeof tokenRegistryTx> &
+  EncodeObjectRecord<typeof marginTx>;
 
 export type SifEncodeObject =
   SifEncodeObjectRecord[keyof SifEncodeObjectRecord];
