e5f59efce3fddf270c7d49f0318184f8e5f63c40
Alan Soares <alanrsoars@gmail.com>
2022-08-10 16:27:40 +1200

    feat: introduce utility hook useSifSignerAddress

--- apps/dex/src/hooks/useSifSigner.ts
+++ apps/dex/src/hooks/useSifSigner.ts
@@ -1,9 +1,29 @@
 import { useSigner } from "@sifchain/cosmos-connect";
+import { invariant } from "@sifchain/ui";
+import { useQuery } from "react-query";
 import { useDexEnvironment } from "~/domains/core/envs";
 
-const useSifSigner = () => {
+function useSifSigner() {
   const { data: env } = useDexEnvironment();
   return useSigner(env?.sifChainId ?? "", { enabled: env !== undefined });
-};
+}
+
+export function useSifSignerAddress() {
+  const { signer } = useSifSigner();
+
+  return useQuery(
+    "sifchain-signer-address",
+    async () => {
+      invariant(signer !== undefined, "Sif signer is not defined");
+
+      const accounts = await signer?.getAccounts();
+
+      return accounts?.[0]?.address ?? "";
+    },
+    {
+      enabled: signer !== undefined,
+    },
+  );
+}
 
 export default useSifSigner;
