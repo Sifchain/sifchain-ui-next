68ac7971e52e3bed3bafaa5f2db385ee4bc3b2ad
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-08-11 18:16:10 +1200

    fix: import renaming

--- apps/dex/src/domains/margin/hooks/index.ts
+++ apps/dex/src/domains/margin/hooks/index.ts
@@ -1,4 +1,5 @@
-export * from "./useOpenMTPMutation";
 export * from "./useCloseMTPMutation";
+export * from "./useMarginHistoryQuery";
+export * from "./useMarginOpenPositionsQuery";
 export * from "./useMarginParamsQuery";
-export * from "./useOpenPositionsQuery";
+export * from "./useOpenMTPMutation";
