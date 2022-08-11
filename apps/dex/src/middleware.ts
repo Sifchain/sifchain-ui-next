a974d97c9ed9b477f0e55ffef55bb0a83cd7e91d
Alan Soares <alanrsoars@gmail.com>
2022-08-11 16:21:30 +1200

    chore: handle case when sifApi

--- apps/dex/src/middleware.ts
+++ apps/dex/src/middleware.ts
@@ -1,18 +1,18 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { hasFeatureFlag } from "./lib/featureFlags";
+// import { hasFeatureFlag } from "./lib/featureFlags";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  if (hasFeatureFlag("margin-standalone")) {
-    return NextResponse.redirect(new URL("/margin", request.url));
-  }
+export function middleware(_request: NextRequest) {
+  // if (hasFeatureFlag("margin-standalone")) {
+  //   return NextResponse.redirect(new URL("/margin", request.url));
+  // }
 
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/swap", "/pools", "/changelog", "/balances", "/legacypools"],
+  matcher: [],
 };
