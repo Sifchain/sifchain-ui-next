4b6f4ef2c8544ec725a2609986f67c45020433da
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-08-11 17:12:38 +1200

    feat: reload page on account changes

--- apps/dex/src/pages/_app.tsx
+++ apps/dex/src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import { useConnect as useCosmConnect } from "@sifchain/cosmos-connect";
 import { ComposeProviders } from "@sifchain/ui";
 import type { AppProps } from "next/app";
-import React, { FC, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { CookiesProvider } from "react-cookie";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { useConnect as useEvmConnect } from "wagmi";
 
 import MainLayout from "~/layouts/MainLayout";
 import { CosmConnectProvider } from "~/lib/cosmConnect";
@@ -19,6 +21,27 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
         },
       }),
   );
+
+  const { activeConnector: activeCosmConnector } = useCosmConnect();
+  const { data: activeEvmConnector } = useEvmConnect();
+
+  const onAccountChange = useCallback(() => window.location.reload(), []);
+
+  useEffect(() => {
+    activeEvmConnector?.connector?.addListener("change", onAccountChange);
+
+    return () => {
+      activeEvmConnector?.connector?.removeListener("change", onAccountChange);
+    };
+  }, [activeCosmConnector, activeEvmConnector?.connector, onAccountChange]);
+
+  useEffect(() => {
+    activeCosmConnector?.addListener("accountchange", onAccountChange);
+
+    return () => {
+      activeCosmConnector?.removeListener("accountchange", onAccountChange);
+    };
+  }, [activeCosmConnector, onAccountChange]);
 
   return (
     <ComposeProviders providers={[CosmConnectProvider, WagmiProvider]}>
