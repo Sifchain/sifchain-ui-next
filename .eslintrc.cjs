4c2c8cd8dc69963a2dce6fc5f9f6d7dc8fc10c36
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-08-12 12:30:06 +1200

    chore: update eslint config to recommended specs

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./apps/*/tsconfig.json", "./packages/*/tsconfig.json"],
+  },
+};
