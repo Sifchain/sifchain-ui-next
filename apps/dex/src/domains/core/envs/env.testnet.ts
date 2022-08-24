edb12ffe573f460957157404910c3bca569f141a
Alan Soares <alanrsoars@gmail.com>
2022-08-25 10:41:10 +1200

    fix: testnet rpc & rest endpoints

--- apps/dex/src/domains/core/envs/env.testnet.ts
+++ apps/dex/src/domains/core/envs/env.testnet.ts
@@ -2,6 +2,6 @@ import type { DexEnvironment } from ".";
 
 export default <DexEnvironment>{
   kind: "testnet",
-  sifnodeUrl: "https://rpc-testnet.sifchain.finance",
+  sifnodeUrl: "https://proxies.sifchain.finance/api/sifchain-testnet/rpc",
   vanirUrl: "https://data.sifchain.finance/beta",
 };
