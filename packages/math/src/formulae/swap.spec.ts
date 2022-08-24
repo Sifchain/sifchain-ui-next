7405fdb2d786d5ff36eaa8c3002f7e4359c1e3b0
Alan Soares <alanrsoars@gmail.com>
2022-08-24 14:13:31 +1200

    feat: implement combined tests for swap & provider fee calculations

--- packages/math/src/formulae/swap.spec.ts
+++ packages/math/src/formulae/swap.spec.ts
@@ -1,50 +1,57 @@
-import BigNumber from "bignumber.js";
-import { test, describe, expect } from "vitest";
+import { describe, expect, test } from "vitest";
 
-import SWAP_TO_ROWAN_CASES from "../../__fixtures__/swap_to_rowan.json";
 import SWAP_FROM_ROWAN_CASES from "../../__fixtures__/swap_from_rowan.json";
-
-import { calculateSwapToRowan } from "./swap";
-
-export type SwapTestCase = {
-  x: string;
-  X: string;
-  Y: string;
-  f: string;
-  r: string;
-  expected: string;
-};
-
-export const parseSwapParams = (params: SwapTestCase) => ({
-  params: {
-    inputAmount: BigNumber(String(params["x"])),
-    inputBalanceInPool: BigNumber(String(params["X"])),
-    outputBalanceInPool: BigNumber(String(params["Y"])),
-    swapFeeRate: BigNumber(String(params["f"])),
-    currentRatioShiftingRate: BigNumber(String(params["r"])),
-  },
-  expected: BigNumber(String(params["expected"])),
-});
+import SWAP_TO_ROWAN_CASES from "../../__fixtures__/swap_to_rowan.json";
+import {
+  calculateSwapFeeFromRowan,
+  calculateSwapFeeToRowan,
+  calculateSwapFromRowan,
+  calculateSwapToRowan,
+} from "./swap";
+import { parseSwapParams, parseSwapTestCase } from "./test-utils";
 
 describe("Swap", () => {
-  describe("to ROWAN", () => {
-    test.each(SWAP_TO_ROWAN_CASES.map(parseSwapParams))(
-      "swapping %s for %s with balance %s has price impact of %s",
-      ({ params, expected }) => {
-        const output = calculateSwapToRowan(params);
+  describe("from ROWAN", () => {
+    test.each(SWAP_FROM_ROWAN_CASES.map(parseSwapTestCase))(
+      "swapping %s with pool balances of %s/%s and fee rate of %s",
+      (x, X, Y, f, r, expectedSwap, expectedFee) => {
+        const { params, expected } = parseSwapParams({
+          x,
+          X,
+          Y,
+          f,
+          r,
+          expectedSwap,
+          expectedFee,
+        });
+        const swapOutput = calculateSwapFromRowan(params);
+        const feeOutput = calculateSwapFeeFromRowan(params);
 
-        expect(output.toPrecision(18)).toEqual(expected.toPrecision(18));
+        expect(swapOutput.toPrecision(18)).toEqual(expected.swap.toPrecision(18));
+        expect(feeOutput.toPrecision(18)).toEqual(expected.fee.toPrecision(18));
       },
     );
   });
 
-  describe("from ROWAN", () => {
-    test.each(SWAP_FROM_ROWAN_CASES.map(parseSwapParams))(
-      "swapping %s for %s with balance %s has price impact of %s",
-      ({ params, expected }) => {
-        const output = calculateSwapToRowan(params);
+  describe("to ROWAN", () => {
+    test.each(SWAP_TO_ROWAN_CASES.map(parseSwapTestCase))(
+      "swapping %s with pool balances of %s/%s and fee rate of %s",
+      (x, X, Y, f, r, expectedSwap, expectedFee) => {
+        const { params, expected } = parseSwapParams({
+          x,
+          X,
+          Y,
+          f,
+          r,
+          expectedSwap,
+          expectedFee,
+        });
+        const swapOutput = calculateSwapToRowan(params);
+        const feeOutput = calculateSwapFeeToRowan(params);
 
-        expect(output.toPrecision(18)).toEqual(expected.toPrecision(18));
+        expect(swapOutput.toPrecision(18)).toEqual(expected.swap.toPrecision(18));
+
+        expect(feeOutput.toPrecision(18)).toEqual(expected.fee.toPrecision(18));
       },
     );
   });
