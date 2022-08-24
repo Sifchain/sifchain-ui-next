771a1d005e0682a139160d82cb615c192a92701f
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-08-25 02:29:25 +1200

    feat: force query to execute and return error, to show correct UI message

--- apps/dex/src/domains/margin/hooks/useMarginParamsQuery.ts
+++ apps/dex/src/domains/margin/hooks/useMarginParamsQuery.ts
@@ -1,5 +1,6 @@
 import useSifnodeQuery from "~/hooks/useSifnodeQuery";
 
 export function useMarginParamsQuery() {
-  return useSifnodeQuery("margin.getParams", [{}], { retry: false });
+  // Force `enable`, to make sure the query returns an error, avoiding infinite "idle" state of `useSifnodeQuery`
+  return useSifnodeQuery("margin.getParams", [{}], { retry: 1, enabled: true });
 }
