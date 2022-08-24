868e964c295c04f97a338bb8c5953c1ee0e5f6fb
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-08-25 02:33:50 +1200

    chore: update error messages based on Brian feedback

--- apps/dex/src/domains/margin/hooks/mutationErrorMessage.ts
+++ apps/dex/src/domains/margin/hooks/mutationErrorMessage.ts
@@ -1,8 +1,9 @@
 export const ACCOUNT_NOT_APPROVED_FOR_TRADING = "Your account has not yet been approved for margin trading.";
 export const ACCOUNT_NOT_ON_CHAIN =
-  "Account does not exist on chain. Send some tokens there before trying to query sequence.";
+  "Insufficient funds. Please import assets onto Sifchain in order to proceed with trading.";
 export const DEFAULT_ERROR_CLOSE_POSITION = "Failed to close margin position. Try again later.";
 export const DEFAULT_ERROR_OPEN = "Failed to open margin position. Try again later.";
 export const NOT_ENOUGH_BALANCE = "You dont have enough balance of the required coin.";
-export const TRADE_MAX_OPEN_POSITIONS_REACHED = "Pool limit for open positions reached. Try again later.";
+export const TRADE_MAX_OPEN_POSITIONS_REACHED =
+  "The selected pool has reached it's limit for open margin positions. Please try again later.";
 export const TRADE_TEMPORARILY_DISABLED = "Trade is temporarily disabled for this pool.";
