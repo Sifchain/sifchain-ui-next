74d986a22e9fbaab3ff414c18614cffa9ec9921f
Alan Soares <alanrsoars@gmail.com>
2022-08-30 15:24:39 +1200

    fix: use sifnode's swapFeeRate to calulate swap fee

--- apps/dex/src/domains/clp/hooks/useSwapSimulation.ts
+++ apps/dex/src/domains/clp/hooks/useSwapSimulation.ts
@@ -1,6 +1,7 @@
 import { Decimal } from "@cosmjs/math";
 import { runCatching } from "@sifchain/common";
-import { useCallback, useEffect, useState } from "react";
+import { Maybe } from "@sifchain/ui";
+import { useQuery } from "react-query";
 
 import useSifnodeQuery from "~/hooks/useSifnodeQuery";
 import { useSifStargateClient } from "~/hooks/useSifStargateClient";
@@ -11,61 +12,61 @@ const COMMON_OPTIONS = {
   enabled: true,
 };
 
-export function useSwapSimulation(fromDenom: string, toDenom: string, fromAmount: string, slippage = 0.01) {
+export function useSwapSimulationQuery(fromDenom: string, toDenom: string, fromAmount: string, slippage = 0.01) {
   const { data: fromToken } = useEnhancedTokenQuery(fromDenom, COMMON_OPTIONS);
   const { data: toToken } = useEnhancedTokenQuery(toDenom, COMMON_OPTIONS);
   const { data: stargateClient } = useSifStargateClient();
   const { data: pmtpParams } = useSifnodeQuery("clp.getPmtpParams", [{}], COMMON_OPTIONS);
+  const { data: swapFeeRateResult } = useSifnodeQuery("clp.getSwapFeeRate", [{}], COMMON_OPTIONS);
 
-  const [result, setResult] = useState<
-    | {
-        rawReceiving: string;
-        minimumReceiving: string;
-        liquidityProviderFee: string;
-        priceImpact: number;
-      }
-    | undefined
-  >(undefined);
+  const compute = (amount = fromAmount) => {
+    const fromPool = fromToken?.pool ?? toToken?.pool;
+    const toPool = toToken?.pool ?? fromToken?.pool;
 
-  const compute = useCallback(
-    (fromAmount = "") => {
-      const fromPool = fromToken?.pool ?? toToken?.pool;
-      const toPool = toToken?.pool ?? fromToken?.pool;
+    const [_, fromAmountDecimal] = runCatching(() => Decimal.fromUserInput(amount, fromToken?.decimals ?? 0));
 
-      const [_, fromAmountDecimal] = runCatching(() => Decimal.fromUserInput(fromAmount, fromToken?.decimals ?? 0));
+    const [__, result] = runCatching(() => {
+      const fromCoin = {
+        denom: fromToken?.denom ?? fromToken?.symbol ?? "",
+        amount: fromAmountDecimal?.atomics ?? "0",
+        poolNativeAssetBalance: fromPool?.nativeAssetBalance ?? "0",
+        poolExternalAssetBalance: fromPool?.externalAssetBalance ?? "0",
+      };
 
-      const [__, result] = runCatching(() =>
-        stargateClient?.simulateSwapSync(
-          {
-            denom: fromToken?.denom ?? fromToken?.symbol ?? "",
-            amount: fromAmountDecimal?.atomics ?? "0",
-            poolNativeAssetBalance: fromPool?.nativeAssetBalance ?? "0",
-            poolExternalAssetBalance: fromPool?.externalAssetBalance ?? "0",
-          },
-          {
-            denom: toToken?.denom ?? toToken?.symbol ?? "",
-            poolNativeAssetBalance: toPool?.nativeAssetBalance ?? "0",
-            poolExternalAssetBalance: toPool?.externalAssetBalance ?? "0",
-          },
-          pmtpParams?.pmtpRateParams?.pmtpPeriodBlockRate,
-          slippage,
-        ),
+      const toCoin = {
+        denom: toToken?.denom ?? toToken?.symbol ?? "",
+        poolNativeAssetBalance: toPool?.nativeAssetBalance ?? "0",
+        poolExternalAssetBalance: toPool?.externalAssetBalance ?? "0",
+      };
+
+      console.log({
+        swapFeeRateResult,
+      });
+
+      return stargateClient?.simulateSwapSync(
+        fromCoin,
+        toCoin,
+        pmtpParams?.pmtpRateParams?.pmtpPeriodBlockRate ?? "0",
+        slippage,
+        Maybe.of(swapFeeRateResult).mapOr("0", (x) => Decimal.fromAtomics(x.swapFeeRate, 18).toString()),
       );
+    });
 
-      return result;
+    return result;
+  };
+
+  const derivedQuery = useQuery(
+    [fromDenom, toDenom, fromAmount, slippage, swapFeeRateResult],
+    compute.bind(null, undefined),
+    {
+      enabled: Boolean(fromToken && toToken && stargateClient && pmtpParams && swapFeeRateResult),
     },
-    [fromToken, toToken, stargateClient, pmtpParams?.pmtpRateParams?.pmtpPeriodBlockRate, slippage],
   );
 
-  useEffect(() => {
-    setResult(compute(fromAmount));
-  }, [fromAmount, compute]);
-
   return {
-    data: result,
+    ...derivedQuery,
     recompute(fromAmount: string) {
       const res = compute(fromAmount);
-      setResult(res);
 
       return res;
     },
