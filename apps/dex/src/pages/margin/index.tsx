73e4ab2bd2b436cc91a5910364f71c40846fe60f
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-09-02 07:23:14 +1200

    feat: add link to margin documentation

--- apps/dex/src/pages/margin/index.tsx
+++ apps/dex/src/pages/margin/index.tsx
@@ -106,7 +106,10 @@ const Margin: NextPage = () => {
       >
         <p className="text-center">
           v1.0 allows for longing ROWAN:TKN and TKN:ROWAN. To see all of v1.0 features along with the upcoming roadmap
-          for Margin, please reference our product documentation **here**
+          for Margin, please reference our product documentation{" "}
+          <Link href="https://sifchain.notion.site/Margin-Trading-v1-0-The-Long-Game-63fe6f60e8094a458047595e4ce18eb9">
+            <a target="_blank">here</a>
+          </Link>
         </p>
       </Modal>
     </>
