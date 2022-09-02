26f4ba391eaa7a653f66f4776c246096957057c4
Alan Soares <alanrsoars@gmail.com>
2022-09-02 14:08:47 +1200

    fix: make hook only update on value change

--- apps/dex/src/utils/useUpdatedAt.ts
+++ apps/dex/src/utils/useUpdatedAt.ts
@@ -2,11 +2,13 @@ import { useEffect, useRef, useState } from "react";
 
 const useUpdatedAt = <T>(value: T) => {
   const isFirstRender = useRef(true);
+  const lastValue = useRef<T | null>(null);
   const [timestamp, setTimestamp] = useState(() => new Date());
 
   useEffect(() => {
-    if (!isFirstRender.current) {
+    if (!isFirstRender.current && JSON.stringify(value) !== JSON.stringify(lastValue.current)) {
       setTimestamp(new Date());
+      return;
     }
 
     isFirstRender.current = false;
