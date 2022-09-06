ce3a0a2e1e0251c9d50eea94857f59cbdb24e1b3
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-09-06 23:02:48 +1200

    fix: add compute reactive dependencies to recalculate fees when the input changes (#158)
    Co-authored-by: Alan Soares <alanrsoars@gmail.com>

--- apps/dex/src/domains/clp/hooks/useSwapSimulation.ts
+++ apps/dex/src/domains/clp/hooks/useSwapSimulation.ts
@@ -2,6 +2,7 @@ import { Decimal } from "@cosmjs/math";
 import { runCatching } from "@sifchain/common";
 import { Maybe } from "@sifchain/utils";
 import { useQuery } from "@tanstack/react-query";
+import { useCallback } from "react";
 
 import useSifnodeQuery from "~/hooks/useSifnodeQuery";
 import { useSifStargateClient } from "~/hooks/useSifStargateClient";
@@ -19,37 +20,40 @@ export function useSwapSimulationQuery(fromDenom: string, toDenom: string, fromA
   const { data: pmtpParams } = useSifnodeQuery("clp.getPmtpParams", [{}], COMMON_OPTIONS);
   const { data: swapFeeRateResult } = useSifnodeQuery("clp.getSwapFeeRate", [{}], COMMON_OPTIONS);
 
-  const compute = (amount = fromAmount) => {
-    const fromPool = fromToken?.pool ?? toToken?.pool;
-    const toPool = toToken?.pool ?? fromToken?.pool;
+  const compute = useCallback(
+    (amount = fromAmount) => {
+      const fromPool = fromToken?.pool ?? toToken?.pool;
+      const toPool = toToken?.pool ?? fromToken?.pool;
 
-    const [_, fromAmountDecimal] = runCatching(() => Decimal.fromUserInput(amount, fromToken?.decimals ?? 0));
+      const [_, fromAmountDecimal] = runCatching(() => Decimal.fromUserInput(amount, fromToken?.decimals ?? 0));
 
-    const [__, result] = runCatching(() => {
-      const fromCoin = {
-        denom: fromToken?.denom ?? fromToken?.symbol ?? "",
-        amount: fromAmountDecimal?.atomics ?? "0",
-        poolNativeAssetBalance: fromPool?.nativeAssetBalance ?? "0",
-        poolExternalAssetBalance: fromPool?.externalAssetBalance ?? "0",
-      };
+      const [__, result] = runCatching(() => {
+        const fromCoin = {
+          denom: fromToken?.denom ?? fromToken?.symbol ?? "",
+          amount: fromAmountDecimal?.atomics ?? "0",
+          poolNativeAssetBalance: fromPool?.nativeAssetBalance ?? "0",
+          poolExternalAssetBalance: fromPool?.externalAssetBalance ?? "0",
+        };
 
-      const toCoin = {
-        denom: toToken?.denom ?? toToken?.symbol ?? "",
-        poolNativeAssetBalance: toPool?.nativeAssetBalance ?? "0",
-        poolExternalAssetBalance: toPool?.externalAssetBalance ?? "0",
-      };
+        const toCoin = {
+          denom: toToken?.denom ?? toToken?.symbol ?? "",
+          poolNativeAssetBalance: toPool?.nativeAssetBalance ?? "0",
+          poolExternalAssetBalance: toPool?.externalAssetBalance ?? "0",
+        };
 
-      return stargateClient?.simulateSwapSync(
-        fromCoin,
-        toCoin,
-        pmtpParams?.pmtpRateParams?.pmtpPeriodBlockRate ?? "0",
-        slippage,
-        Maybe.of(swapFeeRateResult).mapOr("0", (x) => Decimal.fromAtomics(x.swapFeeRate, 18).toString()),
-      );
-    });
+        return stargateClient?.simulateSwapSync(
+          fromCoin,
+          toCoin,
+          pmtpParams?.pmtpRateParams?.pmtpPeriodBlockRate ?? "0",
+          slippage,
+          Maybe.of(swapFeeRateResult).mapOr("0", (x) => Decimal.fromAtomics(x.swapFeeRate, 18).toString()),
+        );
+      });
 
-    return result;
-  };
+      return result;
+    },
+    [fromAmount, fromToken, pmtpParams, slippage, stargateClient, swapFeeRateResult, toToken],
+  );
 
   const derivedQuery = useQuery(
     ["swap-simulation", fromDenom, toDenom, fromAmount, slippage, swapFeeRateResult],
