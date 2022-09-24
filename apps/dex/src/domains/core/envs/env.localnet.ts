1f4680dfb5aa6eaa68aca61698457393bb5aaf44
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-09-25 01:48:54 +1200

    chore: using proxies url for vanir testnet to avoid http content issues

--- apps/dex/src/domains/core/envs/env.localnet.ts
+++ apps/dex/src/domains/core/envs/env.localnet.ts
@@ -3,6 +3,6 @@ import type { DexEnvironment } from ".";
 export default <DexEnvironment>{
   kind: "localnet",
   sifnodeUrl: "https://proxies.sifchain.finance/api/sifchain-testnet/rpc",
-  vanirUrl: "http://65.109.54.24:8080/beta",
+  vanirUrl: "https://proxies.sifchain.finance/api/vanir/testnet/beta",
   registryUrl: "https://registry.sifchain.network",
 };
--- apps/dex/src/domains/core/envs/env.testnet.ts
+++ apps/dex/src/domains/core/envs/env.testnet.ts
@@ -3,5 +3,5 @@ import type { DexEnvironment } from ".";
 export default <DexEnvironment>{
   kind: "testnet",
   sifnodeUrl: "https://proxies.sifchain.finance/api/sifchain-testnet/rpc",
-  vanirUrl: "http://65.109.54.24:8080/beta",
+  vanirUrl: "https://proxies.sifchain.finance/api/vanir/testnet/beta",
 };
