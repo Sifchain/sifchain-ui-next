7d7b0ee84bba527032e5e69ec8dd6a4bff01eee9
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-09-24 13:37:16 +1200

    chore(temporary): default all Data Services APIs to new TestNet

--- apps/dex/src/domains/core/envs/env.devnet.ts
+++ apps/dex/src/domains/core/envs/env.devnet.ts
@@ -3,6 +3,6 @@ import type { DexEnvironment } from ".";
 export default <DexEnvironment>{
   kind: "devnet",
   sifnodeUrl: "https://rpc-devnet.sifchain.finance",
-  vanirUrl: "https://data.sifchain.finance/beta",
+  vanirUrl: "http://dataservices.staging.sifchain.finance:8080/beta",
   registryUrl: "https://registry.sifchain.network",
 };
